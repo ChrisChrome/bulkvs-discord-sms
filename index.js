fdbd80992008c6d754aa6a7dd3240037cfb24813
ChrisChrome <christophercookman@gmail.com>
2023-08-18 20:01:37 -0600

    I DID A THING

---
+++ index.js
@@ -0,0 +1,76 @@
+// a simple express server that takes any URL and console logs it
+const config = require('./config.json');
+const express = require('express');
+const axios = require('axios');
+const Discord = require('discord.js');
+const ffmpeg = require("ffmpeg");
+const hook = new Discord.WebhookClient({ url: config.webhook }, {
+	allowedMentions: {
+		parse: []
+	}
+});
+const fs = require('fs');
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.post('/sms', async (req, res) => {
+	data = req.body;
+	// Get the CNAM record for the number
+	cnam = data.From;
+	await axios.get(`https://cnam.bulkvs.com/?id=${config.bulkvs.id}&did=${data.From}&format=json`, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).then(function (response) {
+		cnam = response.data.name;
+
+	}).catch(function (error) {
+		console.log(error);
+	});
+	if (data.MediaURLs) { // It's an MMS
+		out = {
+			"text": "",
+			"images": []
+		};
+		// Check if theres any text files in MediaURLs
+		// wrap the for loop in an async function so we can use await
+		await (async () => {
+			for (let i = 0; i < data.MediaURLs.length; i++) {
+				if (data.MediaURLs[i].includes(".txt")) {
+					// Get the text file
+					await axios.get(data.MediaURLs[i]).then(function (response) {
+						response.data = response.data.replaceAll("=\n", "");
+						response.data = response.data.replaceAll("=\r\n", "");
+						response.data = response.data.replaceAll("=\r", "");
+						out["text"] = response.data;
+					}).catch(function (error) {
+						console.log(error);
+					})
+				} // Check for jpg and jpeg files
+				else if (data.MediaURLs[i].includes(".jpg") || data.MediaURLs[i].includes(".jpeg") || data.MediaURLs[i].includes(".png") || data.MediaURLs[i].includes(".gif")) {
+					// get file name from URL
+					filename = data.MediaURLs[i].split("/").pop();
+					out['images'].push({ name: filename, attachment: data.MediaURLs[i] });
+				}
+			}
+		})();
+		hook.send({
+			content: out['text'],
+			files: out['images'],
+			username: cnam
+		})
+	} else { // It's an SMS
+		data.Message = data.Message.replaceAll("+", " ");
+		data.Message = decodeURIComponent(data.Message);
+		hook.send({
+			content: data.Message,
+			username: cnam
+		})
+	}
+	res.send("OK");
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
